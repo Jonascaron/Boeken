ab802df029c47a2dace22d7db6caa68f02c88d59
Jonas caron <jonas.caron16@gmail.com>
2025-01-25 01:33:27 +0100

    functions added

---
+++ www/functions.php
@@ -0,0 +1,48 @@
+<?php
+
+function profile_picture($item) {
+    require 'database.php';
+    if (isset($_FILES[$item])) {
+        $img_name = $_FILES[$item]['name'];
+        $tmp_name = $_FILES[$item]['tmp_name'];
+        $id = $_SESSION['id']; // Check if a valid user session exists.
+
+        $img_ex = pathinfo($img_name, PATHINFO_EXTENSION);
+        $img_ex_lc = strtolower($img_ex);
+        $allowed_exs = array('jpg', 'jpeg', 'png');
+
+        if (in_array($img_ex_lc, $allowed_exs)) {
+            $new_name = uniqid("IMG-", true).'.'.$img_ex_lc;
+            $img_upload_path = 'images/users/';
+
+            // Check if the directory exists, and create it if not
+            if (!is_dir($img_upload_path)) {
+                mkdir($img_upload_path, 0777, true); // Recursively create directories with full permissions
+            }
+
+            $full_path = $img_upload_path.$new_name;
+            if (move_uploaded_file($tmp_name, $full_path)) {
+                $sql = "UPDATE users SET image = :image WHERE id = :id";
+                $stmt = $conn->prepare($sql);
+                $stmt->bindparam(':image', $new_name);
+                $stmt->bindparam(':id', $id);
+                if ($stmt->execute()) {
+                    echo "Image uploaded successfully!";
+                } else {
+                    echo "An error occurred while updating the database.";
+                }
+            } else {
+                echo "Error moving the uploaded file.";
+            }
+        } else {
+            echo "Invalid file type. Only JPG, JPEG, and PNG are allowed.";
+        }
+    } else {
+        echo "No file uploaded.";
+    }
+}
+
+function vardump($item) {
+    var_dump($item);
+    die;
+}
