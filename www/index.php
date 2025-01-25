f393bade24c2831c81b4095eb7ecda35c05ce19b
Jonas caron <jonas.caron16@gmail.com>
2025-01-25 12:57:23 +0100

    update index

--- www/index.php
+++ www/index.php
@@ -8,18 +8,9 @@
     $stmt->execute();
     $stmt->rowCount();
     $series = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    include 'head.php';
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/navbar.css">
-    <link rel="stylesheet" href="css/book_container.css">
-
-    <title>Boeken</title>
+    <title>home</title>
 </head>
 <body>
     <nav class="navbar">
