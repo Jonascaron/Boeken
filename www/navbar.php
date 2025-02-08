21ffc4e10a3da7e17c4e677d6c7f9991f56998b1
Jonas caron <jonas.caron16@gmail.com>
2025-02-08 14:43:02 +0100

    navbar done

---
+++ www/navbar.php
@@ -0,0 +1,24 @@
+<nav class="navbar">
+    <div class="logo">
+        <p>Flanagan</p>
+    </div>
+    <div class="form_container">
+        <form action="" method="post" class="form" autocomplete="off">
+            <input type="text" name="search" id="search" placeholder="Zoeken" class="searchbar">
+            <button type="submit" class="searchbutton">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" height="24" viewBox="0 0 24 24" width="24" focusable="false" aria-hidden="true" style="pointer-events: none; display: inherit;"><path clip-rule="evenodd" d="M16.296 16.996a8 8 0 11.707-.708l3.909 3.91-.707.707-3.909-3.909zM18 11a7 7 0 00-14 0 7 7 0 1014 0z" fill-rule="evenodd"></path></svg>
+            </button>
+        </form>
+    </div>
+    <div class="nav">
+        <ul class="nav_list">
+            <li class="nav_item">
+                <a href="index.php">home</a>
+            </li>
+            <li class="nav_item">
+                <a href="book_add.php">boeken toevoegen</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+<div class="spacer"></div>
