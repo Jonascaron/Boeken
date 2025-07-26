22217529f84341e675c03bf8bded8ce1cbb4d430
Jonas caron <jonas.caron16@gmail.com>
2025-07-27 01:24:40 +0200

    head and footer update

--- www/head.php
+++ www/head.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <link rel="icon" href="image/logo/logo.svg" type="svg">
+
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/navbar.css">
     <link rel="stylesheet" href="css/book_container.css">
+    <link rel="stylesheet" href="css/footer.css">
